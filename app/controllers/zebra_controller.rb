801e2035f793fdcf133b4866644890778a499a7f
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-22 17:20:03 +0000

    pushed remaining codes

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -12,8 +12,50 @@ class ZebraController < ApplicationController
   def elephant
     @the_num = params.fetch("number_value")
     @the_result = params.fetch("number_value").to_f * params.fetch("number_value").to_f
-    
+
     render({:template => "calculation_templates/square_results"})
+  end
+
+  def giraffe
+    "<p>Enter a number</p>"
+    render({:template => "calculation_templates/square_root_form"})
+  end
+
+  def tiger
+    @the_num = params.fetch("number_value")
+    @the_result = Math.sqrt(params.fetch("number_value").to_f)
+
+    render({:template => "calculation_templates/square_root_results"})
+  end
+
+  def hen
+    "<p>Enter a number</p>"
+    render({:template => "calculation_templates/random_form"})
+  end
+
+  def chicken
+    @the_min_num = params.fetch("minimum_input").to_f
+    @the_max_num = params.fetch("maximum_input").to_f
+    @the_result = rand(@the_min_num..@the_max_num)
+
+    render({:template => "calculation_templates/random_results"})
+  end
+
+  def deer
+    "<p>Enter a number</p>"
+    render({:template => "calculation_templates/payment_form"})
+  end
+
+  def moose
+    @apr_num = params.fetch("apr_input").to_f
+    @year_num = params.fetch("year_input").to_i
+    @principal_num = params.fetch("principal_input").to_f.round(2)
+
+    @the_numerator = (@apr_num/1200) * @principal_num
+    @the_denominator = 1-(1+@apr_num/100/12)**(-@year_num*12)
+    @the_result = (@the_numerator / @the_denominator).round(2).to_fs(:currency)
+
+    render({:template => "calculation_templates/payment_results"})
   end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,17 @@ Rails.application.routes.draw do
   get("/square/new", {:controller => "zebra", :action => "kitten"})
 
   get("/square/results", {:controller => "zebra", :action => "elephant"})
+
+  get("/square_root/new", {:controller => "zebra", :action => "giraffe"})
+
+  get("/square_root/results", {:controller => "zebra", :action => "tiger"})
+
+  get("/random/new", {:controller => "zebra", :action => "hen"})
+
+  get("/random/results", {:controller => "zebra", :action => "chicken"})
+
+  get("/payment/new", {:controller => "zebra", :action => "deer"})
+
+  get("/payment/results", {:controller => "zebra", :action => "moose"})
+
 end
